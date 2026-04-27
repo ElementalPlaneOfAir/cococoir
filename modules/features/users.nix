11b5afef7a3bae270cd129d2955f0900c666cea8
Nicole Venner <nvenner@protonmail.ch>
2026-04-26 19:55:08 -0600

    allow me to have root ssh access

--- modules/features/users.nix
+++ modules/features/users.nix
@@ -5,22 +5,27 @@
     pkgs,
     inputs,
     ...
-  }: {
+  }: let
+    nicole_ssh_keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINBfMZjr6H4oK3qSBTxjZrMZptWXdzYC6QV4bdS892Ls nicole@vermissian"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN1tyFv2UbkAJMx2U6bp8OwRx5wMpK7/DxSslcPS0sWY nicole@incarnadine"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBQresTdgx3Se26QxvwD/S9SaCRCWL8dvZwZ6IM62b2 nicole@cheddar"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdPzSlJ3TCzPy7R2s2OOBJbBb+U5NY8dwMlGH9wm4Ot nicole@apiarist"
+    ];
+  in {
     programs.fish.enable = true;
 
     users.groups.brad = {};
 
+    users.users.root = {
+      openssh.authorizedKeys.keys = nicole_ssh_keys;
+    };
     users.users.nicole = {
       isNormalUser = true;
       description = "Nicole";
       extraGroups = ["wheel"];
       shell = pkgs.fish;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINBfMZjr6H4oK3qSBTxjZrMZptWXdzYC6QV4bdS892Ls nicole@vermissian"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN1tyFv2UbkAJMx2U6bp8OwRx5wMpK7/DxSslcPS0sWY nicole@incarnadine"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBQresTdgx3Se26QxvwD/S9SaCRCWL8dvZwZ6IM62b2 nicole@cheddar"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdPzSlJ3TCzPy7R2s2OOBJbBb+U5NY8dwMlGH9wm4Ot nicole@apiarist"
-      ];
+      openssh.authorizedKeys.keys = nicole_ssh_keys;
     };
 
     users.users.brad = {
