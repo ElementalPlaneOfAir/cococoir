e417c0d024c49cdfd041e2ba352d77a0024b6e75
Nicole Venner <nvenner@protonmail.ch>
2026-05-07 14:53:41 -0600

    refactor kavita

--- modules/services/kavita.nix
+++ modules/services/kavita.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   cfg = config.cococoir.services.kavita;
 in
@@ -15,24 +15,43 @@ in
       type = lib.types.bool;
       description = "Whether to allow public access to Kavita.";
     };
+
+    tokenKeyFile = lib.mkOption {
+      type = lib.types.path;
+      default = config.clan.core.vars.generators.kavita-token.files.token-key.path;
+      description = ''
+        Path to a file containing the Kavita TokenKey (512+ bits).
+        Defaults to a Clan secret generator.
+      '';
+    };
   };
 
-  config = lib.mkIf cfg.enable {
-    services.kavita = {
+  config = {
+    clan.core.vars.generators.kavita-token = {
+      files.token-key = {};
+      script = ''
+        head -c 64 /dev/urandom | base64 --wrap=0 > $out/token-key
+      '';
+      runtimeInputs = [ pkgs.coreutils ];
+    };
+
+    services.kavita = lib.mkIf cfg.enable {
       enable = true;
+      tokenKeyFile = cfg.tokenKeyFile;
       settings = {
         IpAddresses = "127.0.0.1";
         Port = 5000;
       };
     };
 
-    services.caddy.virtualHosts."${cfg.domain}".extraConfig =
+    services.caddy.virtualHosts."${cfg.domain}".extraConfig = lib.mkIf cfg.enable (
       if cfg.public
       then ''reverse_proxy localhost:5000''
       else ''
         @not_local not remote_ip ${lib.concatStringsSep " " config.cococoir.localNetworks}
         respond @not_local "Forbidden" 403
         reverse_proxy localhost:5000
-      '';
+      ''
+    );
   };
 }
